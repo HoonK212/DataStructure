3268e3fbe193c950b584fab7ccb99172278c59ea
Hoon Kim <b0aeiou@gmail.com>
2020-10-15 23:55:53 +0900

    스택 구현

---
+++ stack/Test.java
@@ -0,0 +1,39 @@
+package dataStructure.stack;
+
+import java.util.EmptyStackException;
+
+class Stack<T> {
+	
+	class Node<T> {
+		private T data;
+		private Node<T> next;
+		
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+	
+	private Node<T> top;
+	
+	public T pop() {
+		if(top == null) {
+			throw new EmptyStackException();
+		}
+		
+		T item = top.data;
+		top = top.next;
+		return item;
+	}
+	
+	public void push(T item) {
+		Node<T> t = new Node<T>(item);
+		t.next = top;
+		top = t;
+	}
+}
+
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+}
